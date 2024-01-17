94c6db8f81a469107f00d504ff7687109c38347f
Otavio Costa <dev.otaviocostao@gmail.com>
2024-01-17 16:56:07 -0300

    Algumas tabelas de comparacao

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title='Analise de eletrônicos')
+st.set_page_config(page_title='Analise de entregas')
 
-st.title('Analise de vendas de eletrônicos')
-st.subheader('Dashboard das vendas por categoria')
+st.title('Analise de entregas do E-Commerce')
+st.subheader('Dashboard')
 st.write('Tabela')
 
-tabela = pd.read_csv('ElectronicsData.csv')
-sub_category= tabela['Sub Category']
-price = tabela['Price']
+tabela = pd.read_csv('ecommerceData.csv')
+#st.table(tabela)
 
-tabela_categoria = pd.DataFrame({'Sub_Category': sub_category, 'Price': price})
+# QUANTIDADE DE PRODUTO POR GALPAO
+galpoes = tabela['Warehouse_block'].value_counts()
+st.table(galpoes)
 
-st.table(tabela_categoria)
+# QUANTIDADE DE PRODUTO POR MEIO DE TRANSPORTE
+transporte = tabela['Mode_of_Shipment'].value_counts()
+st.table(transporte)
+
+# ENTREGUE EM TEMPO POR MEIO DE TRANSPORTE
+col_entregue = tabela['Reached.on.Time_Y.N'] #COLUNA 'Reached.on.Time_Y.N' 
+col_transp = tabela['Mode_of_Shipment'] #COLUNA 'Mode_of_Shipment'
+tab_transp = pd.DataFrame({'transporte': col_transp, 'entregue': col_entregue}) #TABELA COM 'Mode_of_Shipment' e 'Reached.on.Time_Y.N'
+
+transp_entrega = tab_transp.groupby(['transporte']).mean()
+st.table(transp_entrega)
+
+# ENTREGUE EM TEMPO POR GALPAO
+col_galpao = tabela['Warehouse_block']
+tab_galpao = pd.DataFrame({'galpao': col_galpao, 'entregue': col_entregue}) #TABELA COM 'Warehouse_block' e 'Reached.on.Time_Y.N'
+
+galpao_entrega = tab_galpao.groupby(['galpao']).mean()
+st.table(galpao_entrega)
