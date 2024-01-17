c37c299e2e2a8f6aa4288cc70d59426774074850
Otavio Costa <dev.otaviocostao@gmail.com>
2024-01-17 17:10:06 -0300

    Graficos das tabelas criadas

--- main.py
+++ main.py
@@ -4,31 +4,45 @@ import pandas as pd
 st.set_page_config(page_title='Analise de entregas')
 
 st.title('Analise de entregas do E-Commerce')
-st.subheader('Dashboard')
-st.write('Tabela')
 
 tabela = pd.read_csv('ecommerceData.csv')
 #st.table(tabela)
 
+st.subheader('Quantidade de produtos')
 # QUANTIDADE DE PRODUTO POR GALPAO
+st.write('Quantidade de produtos em cada galpão')
 galpoes = tabela['Warehouse_block'].value_counts()
-st.table(galpoes)
+st.bar_chart(galpoes)
 
 # QUANTIDADE DE PRODUTO POR MEIO DE TRANSPORTE
+st.write('Quantidade de produtos em cada meio de transporte')
 transporte = tabela['Mode_of_Shipment'].value_counts()
-st.table(transporte)
+st.bar_chart(transporte)
+
+#COMPARACAO DE ENTREGAS
+st.subheader('Porcentagem de entrega no tempo prometido')
 
 # ENTREGUE EM TEMPO POR MEIO DE TRANSPORTE
+st.write('Porcentagem por meio de transporte')
 col_entregue = tabela['Reached.on.Time_Y.N'] #COLUNA 'Reached.on.Time_Y.N' 
 col_transp = tabela['Mode_of_Shipment'] #COLUNA 'Mode_of_Shipment'
 tab_transp = pd.DataFrame({'transporte': col_transp, 'entregue': col_entregue}) #TABELA COM 'Mode_of_Shipment' e 'Reached.on.Time_Y.N'
 
 transp_entrega = tab_transp.groupby(['transporte']).mean()
-st.table(transp_entrega)
+st.bar_chart(transp_entrega)
 
 # ENTREGUE EM TEMPO POR GALPAO
+st.write('Porcentagem por galpão')
 col_galpao = tabela['Warehouse_block']
 tab_galpao = pd.DataFrame({'galpao': col_galpao, 'entregue': col_entregue}) #TABELA COM 'Warehouse_block' e 'Reached.on.Time_Y.N'
 
 galpao_entrega = tab_galpao.groupby(['galpao']).mean()
-st.table(galpao_entrega)
+st.bar_chart(galpao_entrega)
+
+# ENTREGUE EM TEMPO POR IMPORTANCIA DO PRODUTO
+st.write('Porcentagem por importância do produto')
+col_importancia = tabela['Product_importance']
+tab_importancia = pd.DataFrame({'importancia': col_importancia, 'entregue': col_entregue})
+
+importancia_entrega = tab_importancia.groupby(['importancia']).mean()
+st.bar_chart(importancia_entrega)
